2173847c05ec2785253fd020f1101d420d0ec766
zhiluniao <huangshunle_gxu@163.com>
2020-09-21 00:13:05 +0800

    three_ways_use_json

--- src/serializing.rs
+++ src/serializing.rs
@@ -277,3 +277,78 @@ fn generic_type() {
     // result = Outer { s: 1234567890, ptr: Owned("owned") }
     println!("result = {:?}", result);
 }
+
+fn untyped_example() -> serde_json::Result<()> {
+    let data = r#"
+    {
+        "name": "John Doe",
+        "age": 43,
+        "phones":[
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    }
+    "#;
+    let v: Value = serde_json::from_str(data)?;
+    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+fn typed_example() -> serde_json::Result<()> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    // Parse the string of data into a Person object. This is exactly the
+    // same function as the one that produced serde_json::Value above, but
+    // now we are asking it for a Person as output.
+    let p: Person = serde_json::from_str(data)?;
+
+    // Do things just like with any other Rust data structure.
+    println!("Please call {} at the number {}", p.name, p.phones[0]);
+
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Address {
+    street: String,
+    city: String,
+}
+
+fn print_an_address() -> serde_json::Result<()> {
+    // Some data structure.
+    let address = Address {
+        street: "10 Downing Street".to_owned(),
+        city: "London".to_owned(),
+    };
+
+    // Serialize it to a JSON string.
+    let j = serde_json::to_string(&address)?;
+
+    // Print, write to a file, or send to an HTTP server.
+    println!("{}", j);
+
+    Ok(())
+}
+
+#[test]
+fn three_ways_use_json() {
+    untyped_example();
+    typed_example();
+    print_an_address();
+}
