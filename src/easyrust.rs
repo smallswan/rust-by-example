6b930d517e301286534cc15831546f025b5153e4
zhiluniao <huangshunle_gxu@163.com>
2020-11-21 20:35:39 +0800

    add easyrust.rs

---
+++ src/easyrust.rs
@@ -0,0 +1,308 @@
+#[derive(Debug)]
+struct Names {
+    one_word: Vec<String>,
+    two_words: Vec<String>,
+    three_words: Vec<String>,
+}
+
+struct Company {
+    name: String,
+    ceo: Option<String>,
+}
+
+impl Company {
+    fn new(name: &str, ceo: &str) -> Self {
+        let ceo = match ceo {
+            "" => None,
+            name => Some(name.to_string()),
+        };
+
+        Self {
+            name: name.to_string(),
+            ceo,
+        }
+    }
+
+    fn get_ceo(&self) -> Option<String> {
+        self.ceo.clone()
+    }
+}
+
+fn in_char_vec(char_vec: &Vec<char>, check: char) {
+    println!(
+        "Is {} inside? {}",
+        check,
+        char_vec.iter().any(|&char| char == check)
+    );
+}
+
+#[derive(Debug)]
+struct City {
+    name: String,
+    years: Vec<u32>,
+    populations: Vec<u32>,
+}
+
+impl City {
+    fn new(name: &str, years: Vec<u32>, populations: Vec<u32>) -> Self {
+        Self {
+            name: name.to_string(),
+            years,
+            populations,
+        }
+    }
+
+    fn city_data<F>(&mut self, mut f: F)
+    // We bring in self, but only f is generic F. f is the closure
+    where
+        F: FnMut(&mut Vec<u32>, &mut Vec<u32>), // The closure takes mutable vectors of u32
+                                                // which are the year and population data
+    {
+        f(&mut self.years, &mut self.populations) // Finally this is the actual function. It says
+                                                  // "use a closure on self.years and self.populations"
+                                                  // We can do whatever we want with the closure
+    }
+}
+
+///
+/// 代码来源： https://github.com/Dhghomon/easy_rust#helpful-methods-for-closures-and-iterators
+#[test]
+fn helpful_methods_for_iterators_and_closure() {
+    let years = vec![
+        1372, 1834, 1851, 1881, 1897, 1925, 1959, 1989, 2000, 2005, 2010, 2020,
+    ];
+    let populations = vec![
+        3_250, 15_300, 24_000, 45_900, 58_800, 119_800, 283_071, 478_974, 400_378, 401_694,
+        406_703, 437_619,
+    ];
+    // Now we can create our city
+    let mut tallinn = City::new("Tallinn", years, populations);
+
+    // Now we have a .city_data() method that has a closure. We can do anything we want.
+
+    // First let's put the data for 5 years together and print it.
+    tallinn.city_data(|city_years, city_populations| {
+        // We can call the input anything we want
+        let new_vec = city_years
+            .into_iter()
+            .zip(city_populations.into_iter()) // Zip the two together
+            .take(5) // but only take the first 5
+            .collect::<Vec<(_, _)>>(); // Tell Rust to decide the type inside the tuple
+        println!("{:?}", new_vec);
+    });
+
+    // Now let's add some data for the year 2030
+    tallinn.city_data(|x, y| {
+        // This time we just call the input x and y
+        x.push(2030);
+        y.push(500_000);
+    });
+
+    // We don't want the 1834 data anymore
+    tallinn.city_data(|x, y| {
+        let position_option = x.iter().position(|x| *x == 1834);
+        if let Some(position) = position_option {
+            println!(
+                "Going to delete {} at position {:?} now.",
+                x[position], position
+            ); // Confirm that we delete the right item
+            x.remove(position);
+            y.remove(position);
+        }
+    });
+
+    println!(
+        "Years left are {:?}\nPopulations left are {:?}",
+        tallinn.years, tallinn.populations
+    );
+
+    let months = vec![
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let filtered_months = months
+        .into_iter()
+        .filter(|month| month.len() < 5)
+        .filter(|month| month.contains("u"))
+        .collect::<Vec<&str>>();
+
+    println!("{:?}", filtered_months);
+
+    let company_vec = vec![
+        Company::new("Umbrella Corporation", "Unknown"),
+        Company::new("Ovintiv", "Doug Suttles"),
+        Company::new("The Red-Headed League", ""),
+        Company::new("Stark Enterprises", ""),
+    ];
+
+    let all_the_ceos = company_vec
+        .into_iter()
+        .filter_map(|company| company.get_ceo())
+        .collect::<Vec<String>>();
+    println!("{:?}", all_the_ceos);
+
+    let user_input = vec![
+        "8.9",
+        "Nine point nine five",
+        "8.0",
+        "7.6",
+        "eleventy-twelve",
+    ];
+    let actual_numbers = user_input
+        .into_iter()
+        .filter_map(|input| input.parse::<f32>().ok())
+        .collect::<Vec<f32>>();
+
+    println!("{:?}", actual_numbers);
+
+    let company_vec2 = vec![
+        Company::new("Umbrella Corporation", "Unknown"),
+        Company::new("Ovintiv", "Doug Suttles"),
+        Company::new("The Red-Headed League", ""),
+        Company::new("Stark Enterprises", ""),
+    ];
+
+    let mut result_vec = vec![];
+    company_vec2
+        .iter()
+        .for_each(|company| result_vec.push(company.get_ceo().ok_or("Not found Ceo")));
+
+    for item in result_vec {
+        println!("{:?}", item);
+    }
+
+    let company_vec3 = vec![
+        Company::new("Umbrella Corporation", "Unknown"),
+        Company::new("Ovintiv", "Doug Suttles"),
+        Company::new("The Red-Headed League", ""),
+        Company::new("Stark Enterprises", ""),
+    ];
+
+    let mut result_vec3 = vec![];
+    company_vec3.iter().for_each(|company| {
+        result_vec3.push(
+            company
+                .get_ceo()
+                .ok_or_else(|| format!("Not found Ceo for {}", company.name)),
+        );
+    });
+
+    for item in result_vec3 {
+        println!("{:?}", item);
+    }
+
+    let char_vec = ('a'..'働').collect::<Vec<char>>();
+    in_char_vec(&char_vec, 'i');
+    in_char_vec(&char_vec, '뷁');
+    in_char_vec(&char_vec, '鑿');
+
+    let smaller_vec = ('A'..'z').collect::<Vec<char>>();
+    println!(
+        "All alphabetic? {}",
+        smaller_vec.iter().all(|&x| x.is_alphabetic())
+    );
+    println!(
+        "All less than the character 행? {}",
+        smaller_vec.iter().all(|&x| x < '행')
+    );
+
+    let even_odd = vec!["even", "odd"];
+
+    let even_odd_vec = (0..6)
+        .zip(even_odd.into_iter().cycle())
+        .collect::<Vec<(i32, &str)>>();
+
+    println!("{:?}", even_odd_vec);
+
+    let ten_chars = ('a'..).take(10).collect::<Vec<char>>();
+    let skip_then_ten_chars = ('a'..).skip(1300).take(10).collect::<Vec<char>>();
+
+    println!("{:?}", ten_chars);
+    println!("{:?}", skip_then_ten_chars);
+
+    let some_numbers = vec![9, 6, 9, 10, 11];
+
+    println!(
+        "{}",
+        some_numbers
+            .iter()
+            .fold(0, |total_so_far, next_number| total_so_far + next_number)
+    );
+
+    let num_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    for chunk in num_vec.chunks(3) {
+        println!("{:?}", chunk);
+    }
+
+    println!();
+
+    for window in num_vec.windows(3) {
+        println!("{:?}", window);
+    }
+
+    let rules = "Rule number 1: No fighting. Rule number 2: Go to bed at 8 pm. Rule number 3: Wake up at 6 am.";
+    let rule_locations = rules.match_indices("Rule").collect::<Vec<(_, _)>>(); // This is Vec<usize, &str> but we just tell Rust to do it
+    println!("{:?}", rule_locations);
+
+    let locations = vec![
+        ("Nevis", 25),
+        ("Taber", 8428),
+        ("Markerville", 45),
+        ("Cardston", 3585),
+    ];
+    let mut location_iter = locations.iter().peekable();
+    while location_iter.peek().is_some() {
+        match location_iter.peek() {
+            Some((name, number)) if *number < 100 => {
+                // .peek() gives us a reference so we need *
+                println!("Found a hamlet: {} with {} people", name, number)
+            }
+            Some((name, number)) => println!("Found a town: {} with {} people", name, number),
+            None => break,
+        }
+        location_iter.next();
+    }
+
+    let vec_of_names = vec![
+        "Caesar",
+        "Frodo Baggins",
+        "Bilbo Baggins",
+        "Jean-Luc Picard",
+        "Data",
+        "Rand Al'Thor",
+        "Paul Atreides",
+        "Barack Hussein Obama",
+        "Bill Jefferson Clinton",
+    ];
+
+    let mut iter_of_names = vec_of_names.iter().peekable();
+
+    let mut all_names = Names {
+        // start an empty Names struct
+        one_word: vec![],
+        two_words: vec![],
+        three_words: vec![],
+    };
+
+    while iter_of_names.peek().is_some() {
+        let next_item = iter_of_names.next().unwrap(); // We can use .unwrap() because we know it is Some
+        match next_item.match_indices(' ').collect::<Vec<_>>().len() {
+            // Create a quick vec using .match_indices and check the length
+            0 => all_names.one_word.push(next_item.to_string()),
+            1 => all_names.two_words.push(next_item.to_string()),
+            _ => all_names.three_words.push(next_item.to_string()),
+        }
+    }
+
+    println!("{:?}", all_names);
+}
