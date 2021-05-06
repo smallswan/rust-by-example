3f43f5f8c66ff2602f43cf109507463584563ab6
zhiluniao <huangshunle_gxu@163.com>
2021-05-06 12:44:35 +0800

    encoding

--- src/secret.rs
+++ src/secret.rs
@@ -244,7 +244,7 @@ fn derive_demo() {
 
 /// 使用 PBKDF2 对密码进行加密（salt）和散列（hash）运算
 /// https://rust-cookbook.budshome.com/cryptography/encryption.html
-use data_encoding::HEXUPPER;
+use data_encoding::{DecodeError, HEXUPPER};
 use ring::error::Unspecified;
 use ring::rand::SecureRandom;
 use ring::{digest, pbkdf2, rand};
@@ -288,4 +288,69 @@ fn pbkdf2() {
 
     assert!(should_succeed.is_ok());
     assert!(!should_fail.is_ok());
+}
+
+use percent_encoding::{percent_decode, utf8_percent_encode, AsciiSet, CONTROLS};
+use std::str::Utf8Error;
+use url::form_urlencoded::{byte_serialize, parse};
+
+/// https://url.spec.whatwg.org/#fragment-percent-encode-set
+const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
+
+#[test]
+fn percent_encode() -> Result<(), Utf8Error> {
+    let input = "confident, productive systems programming";
+
+    let iter = utf8_percent_encode(input, FRAGMENT);
+    let encoded: String = iter.collect();
+    assert_eq!(encoded, "confident,%20productive%20systems%20programming");
+
+    let iter = percent_decode(encoded.as_bytes());
+    let decoded = iter.decode_utf8()?;
+    assert_eq!(decoded, "confident, productive systems programming");
+
+    let cn = utf8_percent_encode("通道转兵，强渡乌江，血战娄山关，四渡赤水，通道会议，黎平会议，猴场会议，遵义会议，扎西会议，苟坝会议，会理会议——伟大的转折", FRAGMENT);
+
+    println!("{}", cn);
+
+    let urlencoded: String = byte_serialize("What is ❤?".as_bytes()).collect();
+    assert_eq!(urlencoded, "What+is+%E2%9D%A4%3F");
+    println!("urlencoded:'{}'", urlencoded);
+
+    let decoded: String = parse(urlencoded.as_bytes())
+        .map(|(key, val)| [key, val].concat())
+        .collect();
+    assert_eq!(decoded, "What is ❤?");
+    println!("decoded:'{}'", decoded);
+    Ok(())
+}
+
+#[test]
+fn hex_encode() -> Result<(), DecodeError> {
+    // 编码和解码十六进制
+    let original = b"The quick brown fox jumps over the lazy dog.";
+    let expected = "54686520717569636B2062726F776E20666F78206A756D7073206F76\
+        657220746865206C617A7920646F672E";
+
+    let encoded = HEXUPPER.encode(original);
+    assert_eq!(encoded, expected);
+
+    let decoded = HEXUPPER.decode(&encoded.into_bytes())?;
+    assert_eq!(&decoded[..], &original[..]);
+
+    let cn = HEXUPPER.encode("功成不必在我".as_bytes());
+    println!("{}", cn);
+
+    Ok(())
+}
+
+#[test]
+fn base64_demo() {
+    let hello = b"hello rustaceans";
+    let encoded = base64::encode(hello);
+    let decoded = base64::decode(&encoded).unwrap();
+
+    println!("origin: {}", str::from_utf8(hello).unwrap());
+    println!("base64 encoded: {}", encoded);
+    println!("back to origin: {}", str::from_utf8(&decoded).unwrap());
 }
