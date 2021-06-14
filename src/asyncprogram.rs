04e8ccecb4a2575d31a819a2d4f2cad86af28eae
zhiluniao <huangshunle_gxu@163.com>
2021-06-14 23:07:46 +0800

    unpin

--- src/asyncprogram.rs
+++ src/asyncprogram.rs
@@ -130,6 +130,29 @@ fn why_pin() {
     );
 }
 
+/// Pinning to the Heap
+#[test]
+fn unpin() {
+    use pin_utils::pin_mut; // `pin_utils` is a handy crate available on crates.io
+
+    // A function which takes a `Future` that implements `Unpin`.
+    fn execute_unpin_future(x: impl Future<Output = ()> + Unpin) { /* ... */
+    }
+
+    // let fut = async { /* ... */ };
+    // execute_unpin_future(fut); // Error: `fut` does not implement `Unpin` trait
+
+    // Pinning with `Box`:
+    let fut = async { /* ... */ };
+    let fut = Box::pin(fut);
+    execute_unpin_future(fut); // OK
+
+    // Pinning with `pin_mut!`:
+    let fut = async { /* ... */ };
+    pin_mut!(fut);
+    execute_unpin_future(fut); // OK
+}
+
 #[test]
 fn demo() {
     let future = hello_world();
