f18ffe31985fda47b49c6ad2e547a61f8a0031a8
zhiluniao <huangshunle_gxu@163.com>
2022-07-10 00:06:14 +0800

    mmap(内存映射)

--- src/concurrent.rs
+++ src/concurrent.rs
@@ -13,27 +13,24 @@ mod tests {
     /// https://course.rs/compiler/pitfalls/main-with-channel-blocked.html
     #[test]
     fn drop_send() {
-    
         use std::thread;
-    
+
         let (send, recv) = mpsc::channel();
         let num_threads = 3;
         for i in 0..num_threads {
             let thread_send = send.clone();
             thread::spawn(move || {
-    
                 thread_send.send(i).unwrap();
                 println!("thread {:?} finished", i);
             });
         }
-    
+
         drop(send);
         for x in recv {
             println!("Got: {}", x);
         }
         println!("finished iterating");
     }
-    
 }
 
 #[test]
--- src/io/articles.rs
+++ src/io/articles.rs
@@ -134,4 +134,25 @@ mod tests {
             println!("{}={}", key, value);
         })
     }
+
+    use memmap::Mmap;
+    use std::io::{Error, Write};
+
+    /// https://rusty.rs/files/read-write.html
+    #[test]
+    fn mmap() -> Result<(), Error> {
+        write!(
+            File::create("content.txt")?,
+            "My hovercraft is full of eels!"
+        )?;
+
+        let file = File::open("content.txt")?;
+        let map = unsafe { Mmap::map(&file)? };
+
+        let random_indexes = [0, 1, 2, 19, 22, 10, 11, 29];
+        assert_eq!(&map[3..13], b"hovercraft");
+        let random_bytes: Vec<u8> = random_indexes.iter().map(|&idx| map[idx]).collect();
+        assert_eq!(&random_bytes[..], b"My loaf!");
+        Ok(())
+    }
 }
