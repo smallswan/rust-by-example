6238fe2d5d324084009abeaa372ec12a009563b5
zhiluniao <huangshunle_gxu@163.com>
2022-07-13 23:31:45 +0800

    loop 表达式 + break 语句;

--- src/lib.rs
+++ src/lib.rs
@@ -67,12 +67,32 @@ mod tests {
                 acc += n_squared;
             }
         }
+
         println!("imperative style: {}", acc);
+
+        //loop 表达式 + break 语句
+        // 奇数的平方为奇数，偶数的平方为偶数，依次规律减少判断，以及计算量
+        let mut sum = 0;
+        let mut n = 1;
+        let acc1 = loop {
+            let n_squared = n * n;
+            if n_squared >= upper {
+                // 若大于上限则退出循环
+                break sum;
+            } else {
+                sum += n_squared;
+            }
+            n += 2;
+        };
+
+        println!("loop style: {}", acc1);
+        assert_eq!(acc, acc1);
+
         // 函数式的写法
         let sum_of_squared_odd_numbers: u32 = (0..)
             .map(|n| n * n) // 所有自然数取平方
-            .take_while(|&n| n < upper) // 取小于上限的
-            .filter(|&n| is_odd(n)) // 取奇数
+            .take_while(|&n_squared| n_squared < upper) // 取小于上限的
+            .filter(|&n_squared| is_odd(n_squared)) // 取奇数
             .fold(0, |sum, i| sum + i); // 最后加起来
         println!("functional style: {}", sum_of_squared_odd_numbers);
 
