3b6989105d02aec9201758f70040e5b95a387235
zhiluniao <huangshunle_gxu@163.com>
2023-07-17 21:47:38 +0800

    大文件加解密

--- src/secret.rs
+++ src/secret.rs
@@ -241,6 +241,112 @@ mod tests {
 
         assert_eq!(&plaintext, b"plaintext message");
     }
+
+    use anyhow::anyhow;
+    use chacha20poly1305::{
+        aead::{stream, Aead, NewAead},
+        XChaCha20Poly1305,
+    };
+
+    use std::{
+        fs::{self, File},
+        io::{Read, Write},
+    };
+
+    fn encrypt_large_file(
+        source_file_path: &str,
+        dist_file_path: &str,
+        key: &[u8; 32],
+        nonce: &[u8; 19],
+    ) -> Result<(), anyhow::Error> {
+        let aead = XChaCha20Poly1305::new(key.as_ref().into());
+        let mut stream_encryptor = stream::EncryptorBE32::from_aead(aead, nonce.as_ref().into());
+
+        const BUFFER_LEN: usize = 500;
+        let mut buffer = [0u8; BUFFER_LEN];
+
+        let mut source_file = File::open(source_file_path)?;
+        let mut dist_file = File::create(dist_file_path)?;
+
+        loop {
+            let read_count = source_file.read(&mut buffer)?;
+
+            if read_count == BUFFER_LEN {
+                let ciphertext = stream_encryptor
+                    .encrypt_next(buffer.as_slice())
+                    .map_err(|err| anyhow!("Encrypting large file: {}", err))?;
+                dist_file.write(&ciphertext)?;
+            } else {
+                let ciphertext = stream_encryptor
+                    .encrypt_last(&buffer[..read_count])
+                    .map_err(|err| anyhow!("Encrypting large file: {}", err))?;
+                dist_file.write(&ciphertext)?;
+                break;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn decrypt_large_file(
+        encrypted_file_path: &str,
+        dist: &str,
+        key: &[u8; 32],
+        nonce: &[u8; 19],
+    ) -> Result<(), anyhow::Error> {
+        let aead = XChaCha20Poly1305::new(key.as_ref().into());
+        let mut stream_decryptor = stream::DecryptorBE32::from_aead(aead, nonce.as_ref().into());
+
+        const BUFFER_LEN: usize = 500 + 16;
+        let mut buffer = [0u8; BUFFER_LEN];
+
+        let mut encrypted_file = File::open(encrypted_file_path)?;
+        let mut dist_file = File::create(dist)?;
+
+        loop {
+            let read_count = encrypted_file.read(&mut buffer)?;
+
+            if read_count == BUFFER_LEN {
+                let plaintext = stream_decryptor
+                    .decrypt_next(buffer.as_slice())
+                    .map_err(|err| anyhow!("Decrypting large file: {}", err))?;
+                dist_file.write(&plaintext)?;
+            } else if read_count == 0 {
+                break;
+            } else {
+                let plaintext = stream_decryptor
+                    .decrypt_last(&buffer[..read_count])
+                    .map_err(|err| anyhow!("Decrypting large file: {}", err))?;
+                dist_file.write(&plaintext)?;
+                break;
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_xchacha20_poly1305() {
+        use random::{rngs::OsRng, RngCore};
+        let mut large_file_key = [0u8; 32];
+        let mut large_file_nonce = [0u8; 19];
+        OsRng.fill_bytes(&mut large_file_key);
+        OsRng.fill_bytes(&mut large_file_nonce);
+
+        encrypt_large_file(
+            "large_file.txt",
+            "large_file.crypto",
+            &large_file_key,
+            &large_file_nonce,
+        );
+
+        decrypt_large_file(
+            "large_file.crypto",
+            "large_file_temp.txt",
+            &large_file_key,
+            &large_file_nonce,
+        );
+    }
 }
 
 #[test]
